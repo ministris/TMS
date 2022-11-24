66dcfcc16c27c5e4ef4b4f2f6954ee2e25227696
ministris <ministr-s@yandex.ru>
2022-11-24 09:50:15 +0300

    lesson3

---
+++ Lesson3/Lesson3_1.py
@@ -0,0 +1,6 @@
+one_name= input ("назови себя")
+two_name= input ("Обзови себя")
+new2= f'haliava, {one_name} {two_name}'
+new3= f'haliava, {two_name} ! {one_name}'
+new4='haliava, {0} ! {1}'.format(one_name,two_name)
+print (new4)
