d33ee4f3ef41495f2d9a5e13875d248c255aa3a2
ministris <ministr-s@yandex.ru>
2022-12-01 14:31:58 +0300

    lesson5

--- Lesson5/Lesson5_3.py
+++ Lesson5/Lesson5_3.py
@@ -1,15 +1,11 @@
-def my_f(spisok, slovarik):
-    for i in spisok:
-        if i in slovarik:
-            slovarik[i] += 1
-        else:
-            slovarik[i] = 1
 
-list = [1,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,9,9,9,8,8,8,7,7,4,4,41,5,1,12,2]
-dict1 = {}
-my_f(list, dict1)
-print(dict1)
-
-
+list2 = [1,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,9,9,9,8,8,8,7,7,4,4,41,5,1,12,2]
+dict13 = {}
+for x in list2:
+    if dict13.get(x, None):
+        dict13[x] += 1
+    else:
+        dict13[x] = 1
+print(dict13)
 
 
