6ccb88e1b584a5cb43d4f26fe52d23d75494a7de
ministris <ministr-s@yandex.ru>
2022-12-06 18:20:33 +0300

    HM_task1

---
+++ Lesson5/HM_task1.py
@@ -0,0 +1,12 @@
+def recursive_reverse(some_list):
+    for i in some_list:
+        if some_list == []:
+            return empty
+        else:
+            empty.append(some_list[-1])
+        return empty
+
+
+empty = []
+source = [1, 2, 3, 4, 5]
+print(empty)
--- Lesson5/HM_task2.py
+++ Lesson5/HM_task2.py
@@ -1,12 +1,4 @@
-def rec(some_list):
-    assert (some_list)!=0
-    next = some_list[0]
-    for i in some_list:
-        if next < i:
-            next = i
-    return next
-list5 = [2, 1, 0, 5, 7, 6, 4, 3]
-otvet = rec(list5)
-print(otvet)
+source = [1, 2, 3, 4, 5]
 
-
+c=reverse (source)
+print(c)
